e378871dd04226e820196c0b08c163f3d793ea9c
ElLuchoMan <luchoman96@gmail.com>
2021-02-27 22:30:06 -0500

    Agregado controlador login

--- routes/controllers/auth.js
+++ routes/controllers/auth.js
@@ -1,10 +1,15 @@
-
+const { response } = require('express');
 const crearUsuario = (req, res) => {
     return res.json({
         ok: true,
         msg: 'Crear usuario /new'
     });
 }
-module.exports={
-    crearUsuario
+
+const loginUsuario = (req, res) => {
+    return res.json({
+        ok: true,
+        msg: 'Login usuario /'
+    });
 }
+module.exports = { crearUsuario, loginUsuario }
