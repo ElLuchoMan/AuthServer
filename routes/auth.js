b59c22e258ca828af72d7dbf74116a9669af2573
ElLuchoMan <luchoman96@gmail.com>
2021-02-28 10:53:23 -0500

    Agregado custom middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
 const { crearUsuario, loginUsuario, revalidarToken } = require('./controllers/auth');
 const router = Router();
 //Crear nuevo usuario
@@ -7,11 +8,13 @@ router.post('/new', [
     check('name', 'El nombre es obligatorio').not().isEmpty(),
     check('email', 'El email es obligatorio').isEmail(),
     check('password', 'El password es obligatorio').isLength({ min: 6 }),
+    validarCampos
 ], crearUsuario);
 //Login usuario
 router.post('/', [
     check('email', 'El email es obligatorio').isEmail(),
     check('password', 'El password es obligatorio').isLength({ min: 6 }),
+    validarCampos
 ], loginUsuario);
 //Validar Token
 router.get('/renew', revalidarToken);
--- routes/controllers/auth.js
+++ routes/controllers/auth.js
@@ -1,32 +1,11 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator');
 const crearUsuario = (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    }
-    const { email, name, password } = req.body;
-    console.log(email, name, password);
-
     return res.json({
         ok: true,
         msg: 'Crear usuario /new'
     });
 }
 const loginUsuario = (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    }
-    const { email, password } = req.body;
-    console.log(email, password);
-
     return res.json({
         ok: true,
         msg: 'Login usuario /'
