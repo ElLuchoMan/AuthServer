11057e1a7adfe1f723f3fdee9e3486091c5a05f2
ElLuchoMan <luchoman96@gmail.com>
2021-02-27 21:45:52 -0500

    Agregada petición get

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 //Crear servidor-aplicación de express
 const app = express();
 
+//GET
+app.get('/', ()=>{
+console.log('Holi jeje');
+});
+
 app.listen(4000, () => {
     console.log(`Servidor corriendo en ${4000}`);
 });
