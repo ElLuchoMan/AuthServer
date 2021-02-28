855bef57c24db8beb60c69f1a8d9c234d0d66930
ElLuchoMan <luchoman96@gmail.com>
2021-02-27 21:35:38 -0500

    Inicializado express

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log ('hola de nuevo');
+const express = require('express');
+//Crear servidor-aplicación de express
+const app = express();
+
+app.listen(4000, () => {
+    console.log(`Servidor corriendo en ${4000}`);
+});
