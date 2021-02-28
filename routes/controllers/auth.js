466dd430d5293a86e1afd0ac5301ff7f79c48c71
ElLuchoMan <luchoman96@gmail.com>
2021-02-28 11:50:42 -0500

    Agregada verificación de correo

--- routes/controllers/auth.js
+++ routes/controllers/auth.js
@@ -1,9 +1,33 @@
 const { response } = require('express');
-const crearUsuario = (req, res) => {
-    return res.json({
-        ok: true,
-        msg: 'Crear usuario /new'
-    });
+const Usuario = require('../../models/Usuario');
+const crearUsuario = async (req, res) => {
+    const { email, name, password } = req.body;
+    try {
+        //verificar si no existe un correo igual
+        let usuario = await Usuario.findOne({ email });
+        if (usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Email ya registrado'
+            })
+        }
+        //Crear usuario con el modelo 
+      
+        //Encriptar la contraseña con Hash
+
+        //Generar JWT
+
+        //Crear usuario de DB
+   
+        //Generar respuesta
+
+
+    } catch (error) {
+        return res.status(500).json({
+            ok: false,
+            msg: 'Contacte al administrador'
+        });
+    }
 }
 const loginUsuario = (req, res) => {
     return res.json({
