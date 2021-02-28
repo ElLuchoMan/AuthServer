837af789fc0311dd1e1af10f7cb3a7bb0aa71a14
ElLuchoMan <luchoman96@gmail.com>
2021-02-28 10:34:08 -0500

    Validación completa del login

--- routes/auth.js
+++ routes/auth.js
@@ -6,8 +6,9 @@ const router = Router();
 router.post('/new', crearUsuario);
 //Login usuario
 router.post('/', [
-    check('email','El email es obligatorio').isEmail(),
-] ,loginUsuario);
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password es obligatorio').isLength({ min: 6 }),
+], loginUsuario);
 //Validar Token
-router.get('/renew',revalidarToken);
+router.get('/renew', revalidarToken);
 module.exports = router;
